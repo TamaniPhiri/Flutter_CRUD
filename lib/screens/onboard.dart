c80e761eadbf0b4040bed17f80f53f72f064d849
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 14:33:40 +0200

    changes

---
+++ lib/screens/onboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatelessWidget {
+  const Onboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
