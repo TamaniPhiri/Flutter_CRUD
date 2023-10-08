ee28b52f8874a4e27df230da19d0595193afc20c
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 19:03:34 +0200

    changes

--- lib/screens/intro_screens/intro_one.dart
+++ lib/screens/intro_screens/intro_one.dart
@@ -6,7 +6,7 @@ class IntroOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       decoration: const BoxDecoration(),
       child: const Column(
         mainAxisAlignment: MainAxisAlignment.center,
