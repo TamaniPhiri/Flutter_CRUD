bd60ebfa8efa5527c7d27ec0e2fcb26459f86790
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 19:34:01 +0200

    changes

---
+++ lib/screens/intro_screens/intro_two.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class IntroTwo extends StatelessWidget {
+  const IntroTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
