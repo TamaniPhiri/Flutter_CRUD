a50b879fe3daf2433975bcbb676ed6bd3b16311e
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 20:05:15 +0200

    changes

--- lib/screens/auth_screens/register_screen.dart
+++ lib/screens/auth_screens/register_screen.dart
@@ -10,6 +10,6 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
