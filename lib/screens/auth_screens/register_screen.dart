8ae4afd9300a27cf74a4463fe06d5f3b370aa72b
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 20:26:16 +0200

    changes

--- lib/screens/auth_screens/register_screen.dart
+++ lib/screens/auth_screens/register_screen.dart
@@ -15,37 +15,39 @@ class _RegisterScreenState extends State<RegisterScreen> {
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            children: [
-              const Text(
-                "Welcome",
-                style: TextStyle(color: Colors.white, fontSize: 32),
-              ),
-              const TextField(
-                style: TextStyle(color: Colors.white),
-              ),
-              const TextField(
-                style: TextStyle(color: Colors.white),
-              ),
-              const TextField(
-                style: TextStyle(color: Colors.white),
-              ),
-              InkWell(
-                borderRadius: BorderRadius.circular(20),
-                onTap: () {},
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(20),
-                  child: const Text(
-                    "Sign up",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  "Welcome",
+                  style: TextStyle(color: Colors.white, fontSize: 32),
                 ),
-              )
-            ],
+                const TextField(
+                  style: TextStyle(color: Colors.white),
+                ),
+                const TextField(
+                  style: TextStyle(color: Colors.white),
+                ),
+                const TextField(
+                  style: TextStyle(color: Colors.white),
+                ),
+                InkWell(
+                  borderRadius: BorderRadius.circular(20),
+                  onTap: () {},
+                  child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.all(20),
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
