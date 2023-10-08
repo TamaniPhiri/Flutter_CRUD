7a15650b9b05b5ac5b96681b6015e0747cdb954f
tamani phiri <tamanigabriel0@gmail.com>
2023-10-08 20:04:16 +0200

    changes

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -10,6 +10,6 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
