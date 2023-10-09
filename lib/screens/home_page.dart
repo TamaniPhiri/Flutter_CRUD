687c5262e0cd4d659668e340aa19d6222ea2655f
tamani phiri <tamanigabriel0@gmail.com>
2023-10-09 22:17:56 +0200

    changes

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text(
+          "Home",
+          style: TextStyle(color: Colors.white),
+        )
+      ],
+    );
+  }
+}
